4691ca4c07b7cac4c0b2393438b913ae3daff0d8
ctizzle <charlieta88@gmail.com>
2021-09-28 20:17:29 -0500

    got routines to render

--- src/components/Routines.js
+++ src/components/Routines.js
@@ -1,9 +1,42 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 // import ReactDOM from 'react-dom';
+import api from '../api/api';
 
 const RoutinesComponent = ( ) => {
+    const [routineList, setRoutineList] = useState([]);
+
+    useEffect( async function () {
+        try {
+            const data = await api.makeApiRequest('/routines', 'GET');
+            setRoutineList(data);
+        } catch (error) {
+            console.error(error);
+        }
+    }, [routineList])
+
+    const routinesElement = routineList.map((routine, i) => 
+        <div key={`rou-id-${i}`}>
+            <h1> ROUTINES </h1>
+            <p>Name: {routine.name}</p>
+            <p>Goal: {routine.goal}</p>
+            <p>Creator: {routine.creatorName}</p>
+            {
+                routine.activities.map( activity => 
+                    <>
+                        <p>Activity Name: {activity.name}</p>
+                        <ul>Activity Description: {activity.description}</ul>
+                        <ul>Activity Duration: {activity.duration}</ul>
+                        <ul>Activity Count: {activity.count}</ul>
+                    </>
+                )
+            }
+        </div>
+    )
     return(
-        <h1>We are in the Routines page!</h1>
+        <div className = 'routines-box'>
+            {routinesElement}
+            <h1>We are in the Routines page!</h1>
+        </div>
     )
 }
 
