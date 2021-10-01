09009b31ccd502a2e1586f70f853f5eb0fc01bfd
ctizzle <charlieta88@gmail.com>
2021-09-30 22:48:37 -0500

    Added isLoggedIn and AddActivity.js

--- src/components/Activities.js
+++ src/components/Activities.js
@@ -2,31 +2,34 @@ import React, {useState, useEffect} from 'react';
 // import ReactDOM from 'react-dom';
 import api from '../api/api';
 
-const ActivitiesComponent = ( ) => {
-     const [activityList, setActivityList] = useState([]);
-     useEffect( async function (){
-        try{
+const ActivitiesComponent = () => {
+    
+    const [activityList, setActivityList] = useState([]);
+
+    useEffect( async function (){
+        try {
             const data = await api.makeApiRequest('/activities', 'GET');
             setActivityList(data)
             console.log(data);
-        }catch (error) {
+        } catch (error) {
           console.error(error);
         }},[activityList]);
         
         
-        const activitiesElements = activityList.map((activity,i) => 
-        <div key={`act-id-${i}`}>  
-        <h1> ACTIVITIES </h1>
-        <p> Name :{activity.name} </p>
-        <p>description:{activity.description} </p>
-        </div>);
-      console.log(activitiesElements)
+    const activitiesElements = activityList.map((activity,i) => 
+          <div key={`act-id-${i}`}>  
+            <h1> ACTIVITIES </h1>
+            <p> Name :{activity.name} </p>
+            <p>description:{activity.description} </p>
+          </div>);
+
+    console.log(activitiesElements)
+
     return(
-       <div className = 'activities-box'>
-             {activitiesElements}
-             <h1>We are in the Activities page!</h1>
-           
-       </div>
+      <div className = 'activities-box'>
+            {activitiesElements}
+            <h1>We are in the Activities page!</h1>
+      </div>
     )
 }
 
@@ -42,3 +45,4 @@ export default ActivitiesComponent;
 //       </>
 //   )
 // } <== this was in our routine function for some reason
+// From Charlie: The workshop asked to show the list of activities for the routine
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,28 +1,42 @@
 import React from 'react';
-import {Link} from 'react-router-dom';
+import {Link, useHistory} from 'react-router-dom';
 import './Header.css'
 
 const Header = ({isLoggedIn, setToken}) => {
+  let history = useHistory();
+
   function handleLogout (event) {
     event.preventDefault();
     localStorage.removeItem('token')
     setToken(null);
+    history.push('/Home')
   }
     return (
         <div className = 'header'>
-     <header>
-    <h1>Fitness track.r</h1>
-    </header>
-    <nav className ='navigation'>
-    <ul>
-              <Link to= {"/Routines"}> My Routines </Link>
-              <Link to= {"/Routines"}> Routines </Link>
-              <Link to={"/Home"}> Home </Link>
-              <Link to= {"/Activities"}> Activities </Link>
-              <Link to={"/Login"}> Login </Link>
-              <Link to={"/Register"}> Register </Link>
-    </ul>
-    </nav>
+          <header>
+            <h1>Fitness track.r</h1>
+          </header>
+         { 
+            isLoggedIn ?
+           <nav className ='navigation'>
+              <ul>
+                  <Link to= {"/Routines"}> My Routines </Link>
+                  <Link to= {"/Routines"}> Routines </Link>
+                  <Link to={"/Home"}> Home </Link>
+                  <Link to= {"/Activities"}> Activities </Link>
+                  <button className='logout' onClick={handleLogout}>Logout</button>
+              </ul>
+            </nav> :
+             <nav className ='navigation'>
+             <ul>
+                 <Link to= {"/Routines"}> Routines </Link>
+                 <Link to={"/Home"}> Home </Link>
+                 <Link to= {"/Activities"}> Activities </Link>
+                 <Link to={"/Login"}> Login </Link>
+                 <Link to={"/Register"}> Register </Link>
+             </ul>
+           </nav>
+         }
         </div>
   
   )
